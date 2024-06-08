73ade1eb5493b5200b3ada048e6346d8cf5bb6f6
Qaddis <svyatoslavb1107@gmail.com>
2024-06-08 18:19:37 +0300

    Fixes

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -6,7 +6,7 @@ import ListItem from "@/components/ui/Li/LandingLi"
 import { advs, techs } from "@/data"
 import { useInView } from "framer-motion"
 import { useEffect, useRef, useState } from "react"
-import styles from "./Home.module.scss"
+import classes from "./Home.module.scss"
 
 export default function Home() {
 	const [amount, setAmount] = useState<number>(0.4)
@@ -37,10 +37,10 @@ export default function Home() {
 
 			<hr />
 
-			<section ref={advsRef} className={styles.advantages}>
+			<section ref={advsRef} className={classes.advantages}>
 				<h2 className="heading">Преимущества</h2>
 
-				<ul className={styles.list}>
+				<ul className={classes.list}>
 					{advs.map((item, index) => (
 						<ListItem
 							key={item.title}
@@ -57,10 +57,10 @@ export default function Home() {
 
 			<hr />
 
-			<section ref={techsRef} className={styles.technologies}>
+			<section ref={techsRef} className={classes.technologies}>
 				<h2 className="heading">Технологии</h2>
 
-				<ul className={styles.list}>
+				<ul className={classes.list}>
 					{techs.map((item, index) => (
 						<ListItem
 							key={item.title}
